e758bda3ffee4e3853d127198fa0b5c9f45422c9
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-07-17 16:03:45 +0530

    use-memo assignment 3 solution

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -6,13 +6,20 @@ export const Assignment3 = () => {
     const [items, setItems] = useState([
         { name: 'Chocolates', value: 10 },
         { name: 'Chips', value: 20 },
-        { name: 'Onion', value: 30 },
+        { name: 'Onion', value: 40 },
         { name: 'Tomato', value: 30 },
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue=useMemo(()=>{
+        let totalValue = 0;
+        for (let i = 0; i <items.length; i++) {
+            totalValue = totalValue + items[i].value;
+        }
+        return totalValue;
+    },[items])
+    
     // Your code ends here
     return (
         <div>
@@ -23,5 +30,5 @@ export const Assignment3 = () => {
             </ul>
             <p>Total Value: {totalValue}</p>
         </div>
-    );
+    )
 };
